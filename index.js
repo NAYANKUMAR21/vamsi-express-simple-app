3ee3f0e0347e459b6b74dfa9a192289fe2b783e6
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-01-25 10:11:54 +0530

    added

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+let name="mohan"
+let counter = 0;
+io.on('connection', (socket) => {
+  // Send the current counter value to a new user
+  io.emit("name",name)
+  io.emit("updateCounter",counter)
+  // Listen for button clicks
+  socket.on('increment', () => {
+    // Increment the counter and broadcast the updated value to all clients
+    counter++;
+    io.emit('updateCounter', counter);
+  });
+  socket.on("name",(nam)=>{
+    name+=nam
+    io.emit("name",nam)
+  })
+
+  // You can add more functionality like decrement, reset, etc., in a similar way
+});
+
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
