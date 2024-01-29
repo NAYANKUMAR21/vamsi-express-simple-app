69f46446bf773555f759c324801b12bcae85ed2a
Nayan Kumar <naayaankumar@gmail.com>
2024-01-29 18:04:10 +0530

    file updated make changes in socket to work

--- index.js
+++ index.js
@@ -5,32 +5,30 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-let name="mohan"
+app.use(express.static(__dirname + '/public'));
+let name = 'mohan';
 let counter = 0;
 io.on('connection', (socket) => {
   // Send the current counter value to a new user
-  io.emit("displayer",name)
-  io.emit("updateCounter",counter)
+  io.emit('displayer', name);
+  io.emit('updateCounter', counter);
   // Listen for button clicks
   socket.on('increment', () => {
     // Increment the counter
     counter++;
     io.emit('updateCounter', counter);
   });
-  socket.on("name",(nam)=>{
-    io.emit("displayer",nam)
-  })
+  socket.on('name', (nam) => {
+    io.emit('displayer', nam);
+  });
 
   // i can add more functionality like decrement, reset, etc., in a similar way
-  socket.on("decrement",()=>{
-    counter--
-    io.emit("updateCounter",counter)
+  socket.on('decrement', () => {
+    counter--;
+    io.emit('updateCounter', counter);
+  });
 });
 
-app.use(express.static(__dirname + '/public'));
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(8080, () => {
+  console.log(`Server is Listening on http://localhost:${8080}`);
 });
